a93f26687d928b32380772e9ffcbc6da5147f819
TylerLi177 <95332938+TylerLi177@users.noreply.github.com>
2022-10-09 09:56:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import statistics
-import numpy as np
+from math import isnan
+import csv
 
 letterRange = list(map(chr, range(97, 107)))
-sheetsToRead = ['WTG-A','WTG-B','WTG-C','WTG-D','WTG-E','WTG-F','WTG-G','WTG-H','WTG-I','WTG-J']
+sheetsToRead = ['WTG-A', 'WTG-B', 'WTG-C', 'WTG-D', 'WTG-E', 'WTG-F', 'WTG-G', 'WTG-H', 'WTG-I', 'WTG-J']
 columnsToRead = ['H:X', 'AA:AB', 'AD:AX', 'AZ', 'BC:CI', 'CK:DD', 'DF:DO', 'DS:DZ']
 dataDictionary = {}
+momentDictionary = {}
 
 
 def getDataFromSheet(column, sheet):
     na_values = ["Bad", "Pt Created", "No Data", "Calc Failed", "Tag not Found"]  # add na values if necessary
-    data = pd.read_excel(r'C:\\Users\\tydud\\Downloads\\OSU Hack Ohio Fall 2022 Data_Final.xlsx', sheet_name=sheet,
+    data = pd.read_excel('C:\\Users\\tydud\\Downloads\\OSU Hack Ohio Fall 2022 Data_Final.xlsx', sheet_name=sheet,
                          na_values=na_values, skiprows=range(0, 2), usecols=column)
-
     return data
 
 
@@ -20,43 +21,31 @@ def findMean(data):
     return statistics.mean(data)
 
 
-# TurbineA = open('TurbineA.txt', 'w')
-# TurbineB = open('TurbineB.txt', 'w')
-# TurbineC = open('TurbineC.txt', 'w')
-# TurbineD = open('TurbineD.txt', 'w')
-# TurbineE = open('TurbineE.txt', 'w')
-# TurbineF = open('TurbineF.txt', 'w')
-# TurbineG = open('TurbineG.txt', 'w')
-# TurbineH = open('TurbineH.txt', 'w')
-# TurbineI = open('TurbineI.txt', 'w')
-# TurbineJ = open('TurbineJ.txt', 'w')
 indexLetter = 0
 for sheet in sheetsToRead:
-    dataFile = 'Turbine{letter}.txt'.format(letter=letterRange[indexLetter])
-    print(sheet)
+    print("Sheet " + sheet)
     for item in columnsToRead:
-        print(item)
+        print("Column " + item)
         dataDictionary[item] = getDataFromSheet(item, sheet)
         print(dataDictionary[item])
         columnsInDataFrame = len(dataDictionary[item].columns)
-        numberOfColumns = list(range(1, int(columnsInDataFrame - 1)))
+        numberOfColumns = list(range(1, columnsInDataFrame - 1))
         print(numberOfColumns)
         print("Number of Columns = {numberOfColumns}".format(numberOfColumns=columnsInDataFrame))
         for index in numberOfColumns:
-            print(index)
-            # dataFile.write(str(dataDictionary[item].iloc[:, index]))
-            np.savetxt(dataFile, dataDictionary[item].iloc[:, index], fmt='%d')
-    # dataFile.close()
+            dataFile = pd.DataFrame(dataDictionary[item].iloc[:, index])
+            momentDictionary[index] = pd.DataFrame(dataDictionary[item].iloc[:, index])
+            dataFile.to_csv('Turbine{letter}.csv'.format(letter=letterRange[indexLetter].upper()), index=False)
+        with open('Turbine{letter}.txt'.format(letter=letterRange[indexLetter].upper()), 'w') as f:
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerows(zip(momentDictionary[range(1, 16)]))
     indexLetter += 1
-        
 
-# TurbineA.close()
-# TurbineB.close()
-# TurbineC.close()
-# TurbineD.close()
-# TurbineE.close()
-# TurbineF.close()
-# TurbineG.close()
-# TurbineH.close()
-# TurbineI.close()
-# TurbineJ.close()
+for letter in letterRange:
+    data = pd.read_csv('Turbine{letter}.csv'.format(letter=letter.upper()))
+    means = open('Turbine{letter}.txt'.format(letter=letter.upper()))
+    for column in data:
+        average = findMean(list(column))
+        data.write(str(average) + "\n")
+    data.close()
+
